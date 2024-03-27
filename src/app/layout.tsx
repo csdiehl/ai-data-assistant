199a5df681a28d923dd50534df05dd6f88296c83
Caleb Diehl <cdiehl@ap.org>
2024-03-27 10:48:45 -0700

    add styled-components registry

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { AI } from "./action"
 import "./globals.css"
+import StyledComponentsRegistry from "@/lib/registry"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {/*@ts-ignore*/}
-        <AI>{children}</AI>
+        <AI>
+          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        </AI>
       </body>
     </html>
   )
