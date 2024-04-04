5b889080aaec71c5654a5165c6012c60ad601b15
Caleb Diehl <cdiehl@ap.org>
2024-04-03 17:39:01 -0700

    style up the login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,31 +8,78 @@ import { FormEvent, useEffect } from "react"
 import styled from "styled-components"
 import { useAuth } from "../context"
 import { addData, getData } from "@/firebase/database"
+import { primary } from "@/components/settings"
 
 const Login = styled.button`
   height: 48px;
   border-radius: 8px;
-  background: #fff;
+  border: none;
+  background: ${primary};
   padding: 8px;
-  color: black;
+  color: #fff;
+  font-weight: bold;
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(0.8);
+  }
 `
 
 const Form = styled.form`
-  h1 {
-    color: grey;
-    font-weight: lighter;
+  max-width: 350px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+
+  h1,
+  h2 {
     text-align: center;
-    margin: 16px 0;
+  }
+
+  h1 {
+    font-weight: 300;
+    color: ${primary};
+  }
+
+  h2 {
+    font-weight: lighter;
+    color: grey;
+  }
+
+  p {
+    color: grey;
+    font-weight: medium;
+    font-size: 0.875rem;
+  }
+  a {
+    color: ${primary};
   }
 `
 
 const Container = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
   background: #f5f5f5;
-  height: 100vh;
+  min-height: 100vh;
   width: 100%;
+
+  img {
+    box-shadow: 0 0 10px grey;
+    border-radius: 8px;
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 24px;
+
+    img {
+      width: 80%;
+    }
+  }
 `
 
 export default function Page() {
@@ -75,11 +122,32 @@ export default function Page() {
 
   return (
     <Container>
+      <img
+        alt="a chart generated with this AI tool"
+        width="500px"
+        src="/hero.png"
+      ></img>
       <Form>
         <h1>AI Data Explorer</h1>
+        <h2>
+          Chat with your data and make visualizations, with the help of an LLM
+        </h2>
         <Login onClick={signIn} type="submit">
           Sign in with Google
         </Login>
+        <p>
+          Note: This app is a demo and solo developer project, and development
+          is in progress. You will likely encounter bugs. Known issues are
+          visualizing some time series data, and certain files with unescaped
+          characters. To report an issue or submit a PR,{" "}
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href="https://github.com/csdiehl/ai-data-assistant"
+          >
+            see here.
+          </a>
+        </p>
       </Form>
     </Container>
   )
